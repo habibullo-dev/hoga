1176a818e7efab3f28939453e25e1f13db3d9a8c
Kim Isaac <kimisaac010@gmail.com>
2024-06-11 14:22:35 +0900

    Initial files uploaded

---
+++ static/DOM.js
@@ -0,0 +1,26 @@
+const clockStartBtn = document.getElementById("clockStartBtn")
+const clockStopBtn = document.getElementById("clockStopBtn")
+const clockResetBtn = document.getElementById("clockResetBtn")
+const displayMinutes = document.getElementById("displayMinutes")
+const displaySeconds = document.getElementById("displaySeconds")
+const displayMilisec= document.getElementById("displayMilisec")
+const clockCont = document.getElementById("clockCont")
+
+const milisecXtraDigit = document.getElementById("milisecXtraDigit")
+const secondXtraDigit = document.getElementById("secondXtraDigit")
+
+let currentMinutes = displayMinutes.innerHTML 
+
+let currentMilisec = displayMilisec.innerHTML 
+
+
+/* = document.getElementById("")
+= document.getElementById("")
+= document.getElementById("")
+= document.getElementById("")
+= document.getElementById("")
+= document.getElementById("")
+= document.getElementById("")
+= document.getElementById("")
+= document.getElementById("") */
+
