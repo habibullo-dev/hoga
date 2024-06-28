755f39d1f392cc34e513227fb5127f95ae1710fc
DXM <kimisaac010@gmail.com>
2024-06-28 15:31:20 +0900

    Reconnected endpoint for settings saveDB -needTest

--- website/widget_settings.py
+++ website/widget_settings.py
@@ -5,6 +5,9 @@ db = sqlalchemy.create_engine("mariadb+mariadbconnector://root:@127.0.0.1:3306/f
 
 settings_all_bp = Blueprint("settings_save", __name__)
 
+
+
+
 @settings_all_bp.post("/widget_all_settings")
 def widgets_grab():
     email = request.json.get("userEmail", "") #identify which row to update according to email (unique key)
@@ -22,64 +25,64 @@ def widgets_grab():
                 print("No change in spotify API widget detected.")
                 
             #Query 2 - youtube_setting ⚠️does not exist yet.
-            if bulk_widgets["youtube_setting"]:
+            if bulk_widgets["w-youtube"]:
                 query2 = conn.execute(text("UPDATE user SET youtube_setting=:user_yt_setting WHERE email=:email"),{
                     "email": email,
-                    "user_yt_setting": bulk_widgets["youtube_setting"]
+                    "user_yt_setting": bulk_widgets["w-youtube"]
                 })
             else:
                 print("No change in youtube API widget detected.")
 
             #Query 3 - calendar_setting
-            if bulk_widgets["calendar_setting"]:
+            if bulk_widgets["w-calendar"]:
                 query3 = conn.execute(text("UPDATE user SET calendar_setting=:user_cal_setting WHERE email=:email"),{
                     "email": email,
-                    "user_cal_setting": bulk_widgets["calendar_setting"]
+                    "user_cal_setting": bulk_widgets["w-calendar"]
                 })
             else:
                 print("No change in calendar settings detected.")
 
             #Query 4 - clock_setting
-            if bulk_widgets["clock_setting"]:
+            if bulk_widgets["w-timer"]:
                 query4 = conn.execute(text("UPDATE user SET clock_setting=:user_clock_setting WHERE email=:email"),{
                     "email": email,
-                    "user_clock_setting": bulk_widgets["clock_setting"]
+                    "user_clock_setting": bulk_widgets["w-timer"]
                 })
             else:
                 print("No change in clock setup detected")
 
             #Query 5 - weather_setting ⚠️does not exist yet.
-            if bulk_widgets["weather_setting"]:
+            if bulk_widgets["w-weather"]:
                 query5 = conn.execute(text("UPDATE user SET weather_setting=:user_weather_setting WHERE email=:email"),{
                     "email": email,
-                    "user_weather_setting": bulk_widgets["weather_setting"]
+                    "user_weather_setting": bulk_widgets["w-weather"]
                 })
             else:
                 print("No change in weather widget detected")
 
             #Query 6 - tasklist_setting
-            if bulk_widgets["tasklist_setting"]:
+            if bulk_widgets["w-tasks"]:
                 query6 = conn.execute(text("UPDATE user SET tasklist_setting=:user_task_setting WHERE email=:email"),{
                     "email": email,
-                    "user_task_setting": bulk_widgets["tasklist_setting"]
+                    "user_task_setting": bulk_widgets["w-tasks"]
                 })
             else:
                 print("No change in user task list detected")
 
             #Query 7 - mood_setting ⚠️does not exist yet.
-            if bulk_widgets["mood_setting"]:
+            if bulk_widgets["currentTheme"]:
                 query7 = conn.execute(text("UPDATE user SET mood_setting=:user_mood_setting WHERE email=:email"),{
                     "email": email,
-                    "user_mood_setting": bulk_widgets["mood_setting"]
+                    "user_mood_setting": bulk_widgets["currentTheme"]
                 })
             else:
                 print("No change in moods preferences detected")
 
-            #Query 8 - Gemini_setting ⚠️does not exist yet.
-            if bulk_widgets["mgemini_setting"]:
+            #Query 8 - Gemini_setting 
+            if bulk_widgets["w-search"]:
                 query8 = conn.execute(text("UPDATE user SET gemini_setting=:user_gemini_setting WHERE email=:email"),{
                     "email": email,
-                    "gemini_mood_setting": bulk_widgets["gemini_setting"]
+                    "gemini_setting": bulk_widgets["w-search"]
                 })
             else:
                 print("No change in gemini setup detected")
@@ -90,8 +93,15 @@ def widgets_grab():
             return {"error" : "total widget update or email assessment issue"}, 205
         
 
-#need WIP tag
-#need WIP tag
+""" IMPORTANT
 
+Outside of widget settings, need object permanence for:
+
+Calendar,
+Timer,
+Tasks,
+Spotify (maybe)
+
+ """
 
 
