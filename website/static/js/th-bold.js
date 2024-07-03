2fa6f19e05748cb8560a6818e075e6eabd9f7b56
DXM <kimisaac010@gmail.com>
2024-07-03 15:32:39 +0900

    fixed bold.js logic and bold.html

--- website/static/js/th-bold.js
+++ website/static/js/th-bold.js
@@ -3,7 +3,7 @@ thBold()
 async function thBold(){
 
 
-
+    FXCreateEnergy()
     console.log("Content finished loading. deleting Loading Screen")
     loadingScreen.style.opacity="0";
     setTimeout(()=>{
