07dde75bc46a7182700a1ee4aa9629e8840b9f7a
PA <paoloaquino5@gmail.com>
2024-07-03 13:47:22 +0900

    commit

---
+++ website/static/js/th-cosy.js
@@ -0,0 +1,12 @@
+thCosy()
+
+async function thCosy(){
+
+
+
+    console.log("Content finished loading. deleting Loading Screen")
+    loadingScreen.style.opacity="0";
+    setTimeout(()=>{
+        loadingScreen.innerHTML="";
+    }, 150)
+}
