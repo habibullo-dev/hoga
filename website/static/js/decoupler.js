a8675796eca007f138bf5a498daded5a6578bd44
jongsooy0127 <77057480+jongsooy0127@users.noreply.github.com>
2024-07-03 17:50:28 +0900

    ADMIN dashboard added fetch("")

--- website/static/js/decoupler.js
+++ website/static/js/decoupler.js
@@ -2,7 +2,7 @@
 // Primary Vars 
 window.user = {
     "loggedIn": false,
-    "email": "hogadashboard@gmail.com",
+    "email": "empty.com",
     "name": "",
 };
 window.dbSettingsBulk = {};
@@ -13,20 +13,20 @@ const popupImpText = document.getElementById("popup-imp-text")
 const popupImpBtn1 = document.getElementById("popup-button1")
 const popupImpBtn2 = document.getElementById("popup-button2")
 
-function createUrgentPopUp(_innerText, _btn1, _btn2, _btn1Text, _btn2Text){
-    popupImportant.style.display= "flex";
-    popupImpWindow.style.display= "flex";
+function createUrgentPopUp(_innerText, _btn1, _btn2, _btn1Text, _btn2Text) {
+    popupImportant.style.display = "flex";
+    popupImpWindow.style.display = "flex";
     popupImpText.innerText = _innerText
     popupImpBtn1.addEventListener("click", _btn1)
     popupImpBtn2.addEventListener("click", _btn2)
     popupImpBtn1.innerText = _btn1Text
     popupImpBtn2.innerText = _btn2Text
-    popupImpBtn1.addEventListener("click", ()=>{closeUrgentPopUp(popupImpBtn1, _btn1)})
-    popupImpBtn2.addEventListener("click", ()=>{closeUrgentPopUp(popupImpBtn2, _btn2)})
+    popupImpBtn1.addEventListener("click", () => { closeUrgentPopUp(popupImpBtn1, _btn1) })
+    popupImpBtn2.addEventListener("click", () => { closeUrgentPopUp(popupImpBtn2, _btn2) })
 }
 
-function closeUrgentPopUp(_btn, _btnFunc){
-    popupImportant.style.display= "none";
+function closeUrgentPopUp(_btn, _btnFunc) {
+    popupImportant.style.display = "none";
     popupImpText.innerHTML = ""
     _btn.removeEventListener("click", _btnFunc)
 }
