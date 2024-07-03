e8b150e03a2578c903ddbf33ffd6ce62ae109b73
PA <paoloaquino5@gmail.com>
2024-07-03 13:38:56 +0900

    commit

---
+++ website/static/js/th-minimal.js
@@ -0,0 +1,12 @@
+thMinimal()
+
+async function thMinimal(){
+
+
+
+    console.log("Content finished loading. deleting Loading Screen")
+    loadingScreen.style.opacity="0";
+    setTimeout(()=>{
+        loadingScreen.innerHTML="";
+    }, 150)
+}
