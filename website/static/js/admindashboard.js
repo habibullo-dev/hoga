c8b79ff56e760a312c88a0e2f55a21509e999cca
jongsooy0127 <77057480+jongsooy0127@users.noreply.github.com>
2024-07-04 13:33:14 +0900

    jay changes

--- website/static/js/admindashboard.js
+++ website/static/js/admindashboard.js
@@ -12,23 +12,27 @@ logoutButtonElem.addEventListener("click", (evt) => {
 
 const ctx = document.getElementById('myChart');
 
-new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 2
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
 
-fetch("")
+fetch("/createlinegraph")
+    .then(res => res.json)
+    .then(res => {
+        console.log("RES", res.message)
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'haya', 'heya'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: res.message,
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    })
