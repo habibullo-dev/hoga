0aaf68f19e9b1e4af791055ae4bbbea1c4f2b8a2
jongsooy0127 <77057480+jongsooy0127@users.noreply.github.com>
2024-07-05 14:37:02 +0900

    ISAAC - Fixed skipinstructions

--- website/static/js/instructions.js
+++ website/static/js/instructions.js
@@ -1,61 +1,65 @@
-    
-    console.log("Instructions Loaded")
-    let currentStep = 0;
-    const totalSteps = 5;
 
-    function showStep(step) {
-      for (let i = 0; i < totalSteps; i++) {
-        document.getElementById(`step${i}`).classList.remove("active");
-      }
-      document.getElementById(`step${step}`).classList.add("active");
-      updateProgressBar(step);
-      updateButtons(step);
-    }
+console.log("Instructions Loaded")
+let currentStep = 0;
+const totalSteps = 5;
 
-    function updateProgressBar(step) {
-      const progress = (step / (totalSteps - 1)) * 100;
-      document.getElementById("progressBar").style.width = `${progress}%`;
-    }
+function showStep(step) {
+  for (let i = 0; i < totalSteps; i++) {
+    document.getElementById(`step${i}`).classList.remove("active");
+  }
+  document.getElementById(`step${step}`).classList.add("active");
+  updateProgressBar(step);
+  updateButtons(step);
+}
 
-    function updateButtons(step) {
-      if (step === 0) {
-        document.getElementById("prevButton").style.visibility = "hidden";
-      } else {
-        document.getElementById("prevButton").style.visibility = "visible";
-      }
+function updateProgressBar(step) {
+  const progress = (step / (totalSteps - 1)) * 100;
+  document.getElementById("progressBar").style.width = `${progress}%`;
+}
 
-      if (step === totalSteps - 1) {
-        document.getElementById("nextButton").style.visibility = "hidden";
-      } else {
-        document.getElementById("nextButton").style.visibility = "visible";
-      }
-    }
+function updateButtons(step) {
+  if (step === 0) {
+    document.getElementById("prevButton").style.visibility = "hidden";
+  } else {
+    document.getElementById("prevButton").style.visibility = "visible";
+  }
 
-    function nextStep() {
-      if (currentStep < totalSteps - 1) {
-        currentStep++;
-        showStep(currentStep);
-      }
-    }
+  if (step === totalSteps - 1) {
+    document.getElementById("nextButton").style.visibility = "hidden";
+  } else {
+    document.getElementById("nextButton").style.visibility = "visible";
+  }
+}
 
-    function prevStep() {
-      if (currentStep > 0) {
-        currentStep--;
-        showStep(currentStep);
-      }
-    }
+function nextStep() {
+  if (currentStep < totalSteps - 1) {
+    currentStep++;
+    showStep(currentStep);
+  }
+}
 
-    function skipInstructions() {
-      document.getElementById("instructionCard").style.display = "none";
-      //setTimeout(()=>{delete document.getElementById("instructionCard")}, 300)
-      document.getElementById("popup-special2").style.display = "none";
-    }
+function prevStep() {
+  if (currentStep > 0) {
+    currentStep--;
+    showStep(currentStep);
+  }
+}
 
-    document.getElementById("nextButton").addEventListener("click", nextStep);
-    document.getElementById("prevButton").addEventListener("click", prevStep);
-    document
-      .getElementById("skip")
-      .addEventListener("click", skipInstructions);
-    document
-      .querySelector(".close")
-      .addEventListener("click", skipInstructions);
+function skipInstructions() {
+  console.log("!!CALLING SKIP INSTRUCTIONS")
+  document.getElementById("instructionCard").style.display = "none";
+  //setTimeout(()=>{delete document.getElementById("instructionCard")}, 300)
+  document.getElementById("popup-special2").style.display = "none";
+  if (document.getElementById("popup-special1").childElementCount < 2) {
+    document.getElementById("popup-important").style.display = "none"
+  }
+}
+
+document.getElementById("nextButton").addEventListener("click", nextStep);
+document.getElementById("prevButton").addEventListener("click", prevStep);
+document
+  .getElementById("skip")
+  .addEventListener("click", skipInstructions);
+document
+  .querySelector(".close")
+  .addEventListener("click", skipInstructions);
