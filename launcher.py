6d1ed21fad69ec3252d6c7b906ad1f96a3e76585
DXM <kimisaac010@gmail.com>
2024-07-09 13:57:48 +0900

    building launcher

--- launcher.py
+++ launcher.py
@@ -1,12 +1,18 @@
 import subprocess
 import webbrowser
+import time
+import os
 
 def start_flask():
-    # Start Flask server using subprocess
-    subprocess.Popen(['python', 'main.py'])
+    # Get the absolute path to main.py
+    main_py_path = os.path.join(os.path.dirname(__file__), 'main.py')
+    
+    # Start Flask server using subprocess with the absolute path
+    subprocess.Popen(['python', main_py_path], shell=True)
 
 def open_browser():
     # Open browser to Flask app URL
+    time.sleep(5)
     webbrowser.open('http://localhost:5000')  # Adjust URL/port as needed
 
 if __name__ == '__main__':
