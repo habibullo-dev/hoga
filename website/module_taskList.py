f29fe37618a27163732fadf5b0fec16cc88c4036
deephouse025 <deephouse025@gmail.com>
2024-07-19 14:29:01 -0400

    updated mariadb connectors to pymysql

--- website/module_taskList.py
+++ website/module_taskList.py
@@ -4,7 +4,10 @@ from time import time
 import sqlalchemy
 from sqlalchemy import text
 import json
-db = sqlalchemy.create_engine("mariadb+mariadbconnector://root:@127.0.0.1:3306/final project")
+# db = sqlalchemy.create_engine("mariadb+mariadbconnector://root:@127.0.0.1:3306/final project")
+db = sqlalchemy.create_engine("mariadb+pymysql://root:@127.0.0.1:3306/final project")
+
+
 
 tasklist_bp = Blueprint('tasklist_bp', __name__)
 
